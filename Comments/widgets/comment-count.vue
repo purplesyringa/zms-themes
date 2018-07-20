3999ee20f424c3213d32032a552b2ef0ec361289
Ivanq <imachug@yandex.ru>
2018-07-20 13:42:43 +0300

    [Comments] Add plugin

---
+++ Comments/widgets/comment-count.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="comment-count">
+		<icon name="comment-alt" class="icon" />
+		{{post.comments.length}} comments
+	</div>
+</template>
+
+<style lang="sass" scoped>
+	.icon
+		color: #B10DC9
+</style>
+
+<script type="text/javascript">
+	export default {
+		props: ["post"],
+		name: "comment-count",
+		title: "Comment count",
+		scope: "post"
+	};
+</script>
